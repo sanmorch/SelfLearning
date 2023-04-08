20ac58412b09860e1d5bfacca6945f1127d86236
Aleksandra Morozova <alemorozova@croc.ru>
2023-04-08 16:19:23 +0300

    add navigation menu

--- app/src/main/java/com/example/selflearning/MainActivity.java
+++ app/src/main/java/com/example/selflearning/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.selflearning;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+
+            case R.id.nav_first_course:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FirstCourseFragment()).commit();
+                break;
+
+            case R.id.nav_second_course:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SecondCourseFragment()).commit();
+                break;
+
+            case R.id.nav_third_course:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ThirdCouseFragment()).commit();
+                break;
+
+            case R.id.nav_fourth_course:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FourthCourseFragment()).commit();
+                break;
+
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                break;
+
+            case R.id.nav_about_us:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutUsFragment()).commit();
+                break;
+
+            case R.id.nav_log_out:
+                Toast.makeText(this, "ЗАГЛУШКА ДЛЯ ВЫХОДА ИЗ АККАУНТА", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    protected void init() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
