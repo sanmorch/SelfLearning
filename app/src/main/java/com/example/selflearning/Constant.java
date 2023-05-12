edb17a83b25e883c9693eab284e3ac8e101870e1
Aleksandra Morozova <alemorozova@croc.ru>
2023-05-12 15:15:19 +0300

    return listView for first course

--- app/src/main/java/com/example/selflearning/Constant.java
+++ app/src/main/java/com/example/selflearning/Constant.java
@@ -3,4 +3,5 @@ package com.example.selflearning;
 public class Constant {
     public static final String SUBJECT_NAME = "subject_name";
     public static final String SUBJECT_DESC = "subject_desc";
+    public static final String SUBJECT_ID = "subject_id";
 }
--- app/src/main/java/com/example/selflearning/Fragments/FirstCourseFragment.java
+++ app/src/main/java/com/example/selflearning/Fragments/FirstCourseFragment.java
@@ -1,5 +1,9 @@
 package com.example.selflearning.Fragments;
 
+import static com.example.selflearning.Constant.SUBJECT_DESC;
+import static com.example.selflearning.Constant.SUBJECT_ID;
+import static com.example.selflearning.Constant.SUBJECT_NAME;
+
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -35,26 +39,44 @@ import java.util.List;
 
 public class FirstCourseFragment extends Fragment implements View.OnClickListener {
 
+    // for buttons
     protected MaterialButtonToggleGroup toggleGroup;
-
-    private DatabaseReference databaseReference;
-    private TextView semesterName;
     private Button firstSemesterButton, secondSemesterButton;
+    private TextView semesterName;
+
+    // for DB
+    private DatabaseReference databaseReference;
     private String SUBJECT_KEY = "Subjects";
+
+    // for listView
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> listData;
+    private List<Subject> listSubject;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_first_course, container, false);
         init(view);
-        //getDataFromDB();
+        getDataFromDBFirstSemester();
+        setOnClickItem();
         return view;
     }
 
     //initialize variables
-    protected void init(View view) {
+    private void init(View view) {
 
         semesterName = view.findViewById(R.id.nameSemesterFirstCourse);
+
+        //init for list
+        listView = view.findViewById(R.id.listFirstCourse);
+        listData = new ArrayList<>();
+        listSubject = new ArrayList<>();
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, listData);
+        listView.setAdapter(adapter);
+
 
         //init for toggleGroup
         toggleGroup = view.findViewById(R.id.toggleButtonGroup);
@@ -65,7 +87,37 @@ public class FirstCourseFragment extends Fragment implements View.OnClickListene
         // init for second semester
         secondSemesterButton = view.findViewById(R.id.secondSemesterButton);
         secondSemesterButton.setOnClickListener(this);
+
+        //init for DB
         databaseReference = FirebaseDatabase.getInstance().getReference(SUBJECT_KEY);
+    }
+
+    // for getting data from db to listView
+    private void getDataFromDBFirstSemester() {
+        ValueEventListener vListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (listData.size() > 0) listData.clear();
+                if (listSubject.size() > 0) listSubject.clear();
+                // record data from database from first course, first semester
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Subject subject = ds.getValue(Subject.class);
+                    assert subject != null;
+                    if (subject.course == 1 && subject.semester == 1) {
+                        listData.add(subject.name);
+                        listSubject.add(subject);
+                    }
+                }
+                // notify arrayAdapter about changing data
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        databaseReference.addValueEventListener(vListener);
     }
 
     @Override
@@ -73,10 +125,25 @@ public class FirstCourseFragment extends Fragment implements View.OnClickListene
         switch (view.getId()) {
             case R.id.firstSemesterButton:
                 semesterName.setText("Первый семестер");
+                getDataFromDBFirstSemester();
                 break;
             case R.id.secondSemesterButton:
                 semesterName.setText("Второй семестер");
                 break;
         }
+    }
+
+    private void setOnClickItem() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Subject subjectSelected = listSubject.get(i);
+                Intent intent = new Intent(getActivity(), SubjectPageActivity.class);
+                intent.putExtra(SUBJECT_NAME, subjectSelected.name);
+                intent.putExtra(SUBJECT_DESC, subjectSelected.description);
+                intent.putExtra(SUBJECT_ID, subjectSelected.id);
+                startActivity(intent);
+            }
+        });
     }
 }
